5a508f1f4a4d994e503e2fe408e97fb603d7fc8f
logroa <logroa@umich.edu>
2022-01-21 01:40:01 -0500

    new branch

--- nfteeshirts/nfteeshirts/settings.py
+++ nfteeshirts/nfteeshirts/settings.py
@@ -86,6 +86,7 @@ CORS_ORIGIN_WHITELIST = [
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# switch creds based on env
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
