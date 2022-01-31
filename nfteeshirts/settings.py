66108bb35048a22cb7cb867a154efcbbc10a2595
logroa <logroa@umich.edu>
2022-01-31 13:24:38 -0500

    settings values

--- nfteeshirts/settings.py
+++ nfteeshirts/settings.py
@@ -149,11 +149,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'build/static')
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
