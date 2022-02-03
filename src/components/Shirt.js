58e92d18001298cb9627b1cea8ac36cb776e0a8f
logroa <logroa@umich.edu>
2022-02-02 23:42:58 -0500

    first crack at order form full react components

--- src/components/Shirt.js
+++ src/components/Shirt.js
@@ -14,10 +14,17 @@ class Shirt extends React.Component {
             price: 0,
             available: true,
             hex: '',
-            form_is_open: false
+            form_is_open: false,
+            order_name: "",
+            order_phone_number: "",
+            order_email: "",
+            order_shirts: []
         };
         // will add functionality for picking shirt
+        this.formTypeHandle = this.formTypeHandle.bind(this);
         this.openForm = this.openForm.bind(this);
+        this.orderShirt = this.orderShirt.bind(this);
+        this.formSelectHandle = this.formSelectHandle.bind(this)
     }
 
     componentDidMount() {
@@ -36,16 +43,64 @@ class Shirt extends React.Component {
         });
     };
 
-    openForm() {
-        this.setState({ form_is_open: true });
+    formTypeHandle(field, event) {
+        this.setState({ [field]: event.target.value });
     }
 
-    orderShirt() {
-        
+    formSelectHandle(event) {
+        this.setState(prevState => {
+            return {
+                order_shirts: prevState.order_shirts.push(event.target.value)
+            }
+        });
+    }
+
+    openForm() {
+        if (this.state.form_is_open) {
+            this.setState({ form_is_open: false });
+        }
+        else {
+            this.setState({ form_is_open: true})
+        }
+    }
+
+    orderShirt(event) {
+        const newOrder = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            data: JSON.stringify(
+                {
+                    "email": this.state.order_email,
+                    "name": this.state.order_name,
+                    "phone_number": this.state.phone_number,
+                    "orders": this.state.order_shirts,
+                    "order_price": this.state.price,
+                    "id": this.state.id
+                }
+            )
+        };
+        const api_url = 'https://nfteeshirts.herokuapp.com/api/order/';
+        fetch(api_url, newOrder)
+            .then((response) => {
+                if (!response.ok) throw Error(response.statusText);
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({
+                    form_is_open: false,
+                    order_name: "",
+                    order_phone_number: "",
+                    order_email: "",
+                    order_shirts: []                   
+                });
+            })
+            .catch((error) => console.log(error));
+        event.preventDefault();
     }
 
     render() {
-        const { id, name, pic1_img_url, pic1_title, pic2_img_url, pic2_title, price, available, hex, form_is_open } = this.state;
+        const { id, name, pic1_img_url, pic1_title, pic2_img_url, pic2_title, price, available, hex, form_is_open,
+                order_name, order_phone_number, order_email, order_shirts } = this.state;
 
         let p1_url = "../src/images/".concat(pic1_img_url);
         let p2_url = "../src/images/".concat(pic2_img_url);
@@ -59,7 +114,7 @@ class Shirt extends React.Component {
             let size_options = "";
             for (let x = 0; x < parseInt(num_shirts); ++x) {
                 size_options += (
-                    `<select name="size${x.toString()}" id="shirt-size"  required>
+                    `<select name="size${x.toString()}" id="shirt-size" onChange={(event) => this.formSelectHandle(event)} required>
                         <option value="S">S</option>
                         <option value="M">M</option>
                         <option value="L">L</option>
@@ -75,9 +130,9 @@ class Shirt extends React.Component {
             form = (
                 <div id="form">
                     <form id="order-shirt" onSubmit={(event) => this.orderShirt(event)}>
-                        Name <input type="text" name="name" required/>
-                        Email <input type="text" name="email" required/>
-                        Phone Number <input type="text" name="phone_number" required/>
+                        Name <input type="text" name="name" onChange={(event) => this.formTypeHandle({order_name}, event)} required/>
+                        Email <input type="text" name="email" onChange={(event) => this.formTypeHandle({order_email}, event)} required/>
+                        Phone Number <input type="text" name="phone_number" onChange={(event) => this.formTypeHandle({order_phone_number}, event)} required/>
                         How many?   <select name="shirt-num" id="shirt-num" onChange={sizeRenderer()} required>
                             <option value="1">1</option>
                             <option value="2">2</option>
